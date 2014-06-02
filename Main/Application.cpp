8d961a210fc24ed3997ea5e64329443401bb7689
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2014-06-02 22:56:30 +0100

    Upgrade to new wxWidgets 3.0 wxStandardPaths API

--- Main/Application.cpp
+++ Main/Application.cpp
@@ -36,7 +36,6 @@ namespace TrueCrypt
 
 	FilePath Application::GetConfigFilePath (const wxString &configFileName, bool createConfigDir)
 	{
-		wxStandardPaths stdPaths;
 		DirectoryPath configDir;
 		
 		if (!Core->IsInPortableMode())
@@ -46,6 +45,7 @@ namespace TrueCrypt
 			configPath.Normalize();
 			configDir = wstring (configPath.GetFullPath());
 #else
+			wxStandardPaths stdPaths;
 			configDir = wstring (stdPaths.GetUserDataDir());
 #endif
 		}
@@ -61,12 +61,12 @@ namespace TrueCrypt
 
 	DirectoryPath Application::GetExecutableDirectory ()
 	{
-		return wstring (wxFileName (wxStandardPaths().GetExecutablePath()).GetPath());
+		return wstring (wxFileName (wxStandardPaths::Get().GetExecutablePath()).GetPath());
 	}
 
 	FilePath Application::GetExecutablePath ()
 	{
-		return wstring (wxStandardPaths().GetExecutablePath());
+		return wstring (wxStandardPaths::Get().GetExecutablePath());
 	}
 
 	void Application::Initialize (UserInterfaceType::Enum type)
