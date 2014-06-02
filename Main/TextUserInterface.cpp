0f3460377a6306775dbf0c6b78e4d18deb00c1a5
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2014-06-02 22:55:44 +0100

    Upgrade wstring and wchar handling to wxWidgets 3.0

--- Main/TextUserInterface.cpp
+++ Main/TextUserInterface.cpp
@@ -116,7 +116,7 @@ namespace TrueCrypt
 			for (size_t i = 0; i < length && i < VolumePassword::MaxSize; ++i)
 			{
 				passwordBuf[i] = (wchar_t) passwordStr[i];
-				const_cast <wchar_t *> (passwordStr.c_str())[i] = L'X';
+				const_cast <wchar_t *> (passwordStr.wc_str())[i] = L'X';
 			}
 
 			if (verify && verPhase)
@@ -763,8 +763,8 @@ namespace TrueCrypt
 
 				ShowString (wxString::Format (L"\rDone: %7.3f%%  Speed: %9s  Left: %s         ",
 					100.0 - double (options->Size - progress.SizeDone) / (double (options->Size) / 100.0),
-					speed > 0 ? SpeedToString (speed).c_str() : L" ",
-					speed > 0 ? TimeSpanToString ((options->Size - progress.SizeDone) / speed).c_str() : L""));
+					speed > 0 ? SpeedToString (speed).wc_str() : L" ",
+					speed > 0 ? TimeSpanToString ((options->Size - progress.SizeDone) / speed).wc_str() : L""));
 			}
 
 			Thread::Sleep (100);
